41625ab064abf1218853765a41071ab4b7133bd2
Matthew Leach <dev@mattleach.net>
2024-08-05 20:02:01 +0100

    birboard: implement display
    Implement display for a bitboard, so a chess board can be shown.

--- src/position.rs
+++ src/position.rs
@@ -7,8 +7,33 @@ mod movegen;
 
 #[derive(Clone, Copy, PartialEq)]
 #[repr(transparent)]
-struct BitBoard {
+pub struct BitBoard {
     inner: u64,
+}
+
+impl Display for BitBoard {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let b = format!("{:064b}", self.inner).into_bytes();
+        for (b, rank) in b.chunks(8).zip(Rank::iter().rev()) {
+            write!(f, "{:?} ", rank)?;
+            for v in b.iter().rev() {
+                write!(f, "{} ", match v {
+                    b'0' => '.',
+                    b'1' => '1',
+                    _ => unreachable!()
+                })?;
+            }
+
+            write!(f, "\n")?;
+        }
+
+        write!(f, "  ")?;
+        for file in File::iter() {
+            write!(f, "{:?} ", file)?;
+        }
+
+        Ok(())
+    }
 }
 
 #[derive(Clone, PartialEq)]
