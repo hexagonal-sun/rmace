4b3b2dacc4c1733bf7bbaa6b746dba399563a974
Matthew Leach <dev@mattleach.net>
2024-08-17 21:30:35 +0100

    mmove: improve debug output

--- src/mmove.rs
+++ src/mmove.rs
@@ -1,14 +1,31 @@
+use std::fmt::Debug ;
+
 use crate::{piece::Piece, position::locus::Locus};
 
-#[derive(Clone, Copy, PartialEq, Debug)]
+#[derive(Clone, Copy, PartialEq)]
 pub struct PieceMove {
     pub piece: Piece,
     pub src: Locus,
     pub dst: Locus,
 }
 
-#[derive(Clone, Copy, PartialEq, Debug)]
+impl Debug for PieceMove {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?} -> {:?}", self.src, self.dst)
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
 pub enum Move {
     Move(PieceMove),
     Attack(PieceMove, Piece),
+}
+
+impl Debug for Move {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Move::Move(m) => write!(f, "{m:?}"),
+            Move::Attack(m, p) => write!(f, "{m:?} takes {p:?}"),
+        }
+    }
 }
