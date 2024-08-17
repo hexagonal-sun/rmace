7e6a7d8df9044157af9b39c9560de4e0705cfd7a
Matthew Leach <dev@mattleach.net>
2024-08-17 21:30:51 +0100

    main: show a small start of a game

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
-use position::{locus::Locus, movegen::knight::ATTACK_KNIGHT, Position};
+use position::Position;
 
+mod mmove;
 pub mod piece;
 mod position;
 
 fn main() {
-    let pos = Position::default();
+    let mut pos = Position::default();
 
     println!("{pos}");
+
+    println!("Moves: {:#?}", pos.movegen());
+
+    pos.make_move(pos.movegen()[1]);
+
+    println!("{pos}");
+
+    println!("Moves: {:#?}", pos.movegen());
+
+    pos.make_move(pos.movegen()[2]);
+
+    println!("{pos}");
+    println!("Moves: {:#?}", pos.movegen());
+    pos.make_move(pos.movegen()[6]);
+    println!("{pos}");
+    println!("Moves: {:#?}", pos.movegen());
 }
