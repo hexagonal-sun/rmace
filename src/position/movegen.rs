438d9ed28007825a7e86fbf05e1a0a0212a2de14
Matthew Leach <dev@mattleach.net>
2024-09-03 20:42:26 +0100

    movegen: add further perft tests

--- src/position/movegen.rs
+++ src/position/movegen.rs
@@ -168,4 +168,52 @@ mod tests {
 
         assert_eq!(perft_res, 4085603);
     }
+
+    #[test]
+    fn perft_pos2() {
+        let perft_res = Position::from_fen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - ")
+            .unwrap()
+            .perft(5)
+            .iter()
+            .fold(0, |accum, (_, x)| accum + x);
+
+        assert_eq!(perft_res, 674624);
+    }
+
+    #[test]
+    fn perft_pos3() {
+        let perft_res =
+            Position::from_fen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1")
+                .unwrap()
+                .perft(4)
+                .iter()
+                .fold(0, |accum, (_, x)| accum + x);
+
+        assert_eq!(perft_res, 422333);
+    }
+
+    #[test]
+    fn perft_pos4() {
+        let perft_res =
+            Position::from_fen("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8")
+                .unwrap()
+                .perft(3)
+                .iter()
+                .fold(0, |accum, (_, x)| accum + x);
+
+        assert_eq!(perft_res, 62379);
+    }
+
+    #[test]
+    fn perft_pos5() {
+        let perft_res =
+            Position::from_fen("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10 ")
+                .unwrap()
+                .perft(4)
+                .iter()
+                .fold(0, |accum, (_, x)| accum + x);
+
+        assert_eq!(perft_res,  3894594);
+    }
+
 }
