5ec1cf3c1fc1e9343e1db1a57164683ca1ac9d78
Matthew Leach <dev@mattleach.net>
2024-09-08 18:57:28 +0100

    search: make `should_exit` a member of `Search`
    Don't pass arround a reference to the should_exit flag, include it as
    part of the `Search` struct.

--- src/search.rs
+++ src/search.rs
@@ -19,27 +19,26 @@ pub struct Search {
     pos: Position,
     deadline: Option<Duration>,
     nodes: u32,
+    should_exit: Arc<AtomicBool>,
 }
 
 const INF: i32 = i32::MAX - 2;
 
 impl Search {
     pub fn go(mut self) -> Move {
-        let should_exit = Arc::new(AtomicBool::new(false));
-
         let (tx, rx) = mpsc::channel();
         let deadline = self.deadline.unwrap_or(Duration::from_secs(5));
+        let should_exit = self.should_exit.clone();
 
         {
-            let should_exit = should_exit.clone();
             thread::spawn(move || {
                 let mut depth = 1;
                 loop {
                     let mut pv = Vec::with_capacity(depth);
                     self.nodes = 0;
-                    let best_score = self.search(-INF, INF, depth as u32, &mut pv, &should_exit);
+                    let best_score = self.search(-INF, INF, depth as u32, &mut pv);
 
-                    if should_exit.load(Ordering::Relaxed) {
+                    if self.should_exit.load(Ordering::Relaxed) {
                         return;
                     }
 
@@ -85,7 +84,6 @@ impl Search {
         beta: i32,
         depth: u32,
         pv: &mut Vec<Move>,
-        should_exit: &Arc<AtomicBool>,
     ) -> i32 {
         if depth == 0 {
             let eval = Evaluator::eval(&self.pos);
@@ -108,11 +106,11 @@ impl Search {
 
         for m in mmoves {
             let token = self.pos.make_move(m);
-            let eval = -self.search(-beta, -alpha, depth - 1, &mut local_pv, should_exit);
+            let eval = -self.search(-beta, -alpha, depth - 1, &mut local_pv);
             self.pos.undo_move(token);
 
             // Timeout detection.
-            if (self.nodes & 0xfff == 0xfff) && should_exit.load(Ordering::Relaxed) {
+            if (self.nodes & 0xfff == 0xfff) && self.should_exit.load(Ordering::Relaxed) {
                 return 0;
             }
 
@@ -145,6 +143,7 @@ impl SearchBuilder {
                 pos,
                 deadline: None,
                 nodes: 0,
+                should_exit: Arc::new(AtomicBool::new(false)),
             },
         }
     }
